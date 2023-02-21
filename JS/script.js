7793a300b5b8ed95816d457c7c4db729f4f713bf
Danya <cheberyak123@gmail.com>
2023-02-21 22:26:34 +0200

    JS/HM2/alert

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,89 @@
-const button = document.querySelector('.button')
-const mon = document.querySelector('.money')
-const input1 = document.querySelector('.input1')
-const input2 = document.querySelector('.input2')
-const input3 = document.querySelector('.input3')
+const door = document.querySelector('.door');
+const opena = document.querySelector('.open');
+const clos = document.querySelector('.close');
+const signal = document.querySelector('.signal');
+const on = document.querySelector('.on');
+const off = document.querySelector('.off');
+const alerts = document.querySelector('.alert');
+  
+  let visible = false;
 
-button.addEventListener('click', function(){
-    mon.innerText = 'Hello, where is my money?';
-    const val1 = Number(input1.value);
-    const val2 = Number(input2.value);
-    const val3 = Number(input3.value);
-    let val4 = Math.pow(val1, 2);
-    let val5 = Math.pow(val2, 2);
-    let val6 = Math.pow(val3, 2);
-
-    if (isNaN(val1) || isNaN(val2) || isNaN(val3)) {
-        console.log('Некоректно написаны размеры, треугольник не возможно создать')
-    } else if (val1 === val2 && val2 === val3) {
-        console.log('Равносторонний');
-    } else if (val1 + val2 < val3 || val3 + val2 < val1 || val1 + val3 < val2) {
-        console.log('Треугольник не возможно создать');
-    } else if (val1 === val2 || val2 === val3 || val1 === val3) {
-        console.log('Равнобедренный');
-    } else if (val4 + val5 == val6 || val6 + val5 == val4 || val4 + val6 == val5) {
-        console.log('Египетский');
+signal.addEventListener('click', function() {
+    visible = !visible
+    on.style.display = visible ? 'block' : 'none';
+    if (on.style.display == 'block') {
+        off.style.display = 'none';
     } else {
-        console.log('Обычный треугольник');
+        off.style.display = 'block';
     }
+    if(opena.style.display == 'block' && alerts.style.display == 'block'){
+        on.style.display = 'none';
+        alerts.style.display = 'none'
+        off.style.display = 'block';
+      }else if(opena.style.display == 'block' && on.style.display == 'block'){
+        alerts.style.display = 'block';
+        on.style.display = 'none';
+        off.style.display = 'none';
+      }
 })
+
+let avisible = false;
+door.addEventListener('click', function() {
+    avisible = !avisible
+    clos.style.display = avisible ? 'none' : 'block';
+    if (clos.style.display == 'none') {
+        opena.style.display = 'block';
+    } else {
+        opena.style.display = 'none';
+    }
+    if(opena.style.display == 'block' && on.style.display == 'block'){
+        alerts.style.display = 'block';
+        on.style.display = 'none';
+        off.style.display = 'none';
+    } else if (off.style.display == 'block' && opena.style.display == 'none') {
+        off.style.display == 'block';
+    }else if(opena.style.display == 'none' && on.style.display == 'none'){
+        on.style.display = 'block';
+        alerts.style.display = 'none';
+        off.style.display = 'none';
+    } 
+})
+
+// let visOne = false;
+// let vısTwo = false;
+
+// signal.addEventListener('click', function num() {
+//     vısTwo = !vısTwo
+//     on.style.display = vısTwo ? 'block' : 'none';
+//     off.style.display = vısTwo ? 'none' : 'block';
+//     if(opena.style.display == 'block' && alerts.style.display == 'block'){
+//       on.style.display = 'none';
+//       alerts.style.display = 'none'
+//       off.style.display = 'block';
+//     }else if(opena.style.display == 'block' && on.style.display == 'block'){
+//       alerts.style.display = 'block';
+//       on.style.display = 'none';
+//       off.style.display = 'none';
+//     }
+
+    
+
+    
+// })
+// door.addEventListener('click', function() {
+//     visOne = !visOne
+//     clos.style.display = visOne ? 'none' : 'block';
+//     opena.style.display = visOne ? 'block' : 'none';
+//     if(opena.style.display == 'block' && on.style.display == 'block'){
+//       alerts.style.display = 'block';
+//       on.style.display = 'none';
+//       off.style.display = 'none';
+//     } else if (off.style.display == 'block' && opena.style.display == 'none') {
+//         off.style.display == 'block';
+//     }else if(opena.style.display == 'none' && on.style.display == 'none'){
+//       on.style.display = 'block';
+//       alerts.style.display = 'none';
+//       off.style.display = 'none';
+//     } 
+    
+// })
