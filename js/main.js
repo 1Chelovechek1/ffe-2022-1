e96492bc70cc7354c8fc35bacfde7667597e7398
Danyil Cheberiak <cheberyak123@gmail.com>
2023-05-23 00:40:22 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const a = "asd";
+const b = true;
+const c = 231;
+
+const mobile = { 
+    name: 'Iphone10',
+    color: 'black',
+    price: '15000грн',
+    phoneStatus: 'second-hand'
+};
+console.log( `Name: ${mobile.name}, Price: ${mobile.price}, phoneStatus: ${mobile.phoneStatus}` );
